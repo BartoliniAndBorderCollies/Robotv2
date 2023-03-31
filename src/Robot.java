92eb030bc51db68111c2404d08eff2beba19c589
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-03-31 10:02:49 +0200

    class Robot, basic functionalities

--- src/Robot.java
+++ src/Robot.java
@@ -1,14 +1,31 @@
 public class Robot {
 
-    // robot ma miec poziom energii
+    // robot ma miec poziom energii wartośc od 0 do 100;
+    private int energyLevel;
+    private String name;
+    private boolean isOn;
+
+    public Robot(String name) {
+        this.name = name;
+        energyLevel = 100;
+        isOn = true;
+    }
+
     // robot ma wykonywać ruch
+    public void move() {
+        // bedziemy wykorzystywać enuma
+    }
+
     // robota można włączyć
+    public void turnOn(){
+        isOn = true;
+    }
     // robota można wyłączyć
-    // można sprawdzić poziom energii robota
-
-
-
-
-
-
+    public void turnOff(){
+        isOn = false;
+    }
+// można sprawdzić poziom energii robota
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
 }
