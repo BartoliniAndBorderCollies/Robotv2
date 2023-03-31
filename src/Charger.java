64e6e71caf9bae254c89d3fab482922d77ea9d71
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-03-31 10:02:12 +0200

    class Charger, basic funcionalities

--- src/Charger.java
+++ src/Charger.java
@@ -1,10 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Charger {
 
-    // możliwośc stworzenia ładowarki
-    // możliwośc podłączenie ładowarki, a tym samym ładowania robota
-    // ładowanie odbywa się ileś tam procent na turę
+
+    // możliwośc podłączenie ładowarki do robota
+    private List<Robot> robots = new ArrayList<>();
+
+    public void plugInRobot(Robot robot) {
+        robots.add(robot);
+    }
+
+    // TODO: ładowanie robota
+    public void chargeRobot() {
+        //robot.charge();
+        // ładowanie odbywa się ileś tam procent na turę
+    }
+
     // możliwość odłączenia ładowarki
+    public void unplugRobot(Robot robot) {
+        robots.remove(robot);
 
+    }
 
-
+    // możliwość zwracania robotów
+    public List<Robot> getRobots() {
+        return robots;
+    }
 }
