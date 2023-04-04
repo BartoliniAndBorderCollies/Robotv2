3467c0d4b1e231a34107a3eb103bd3e51acacf01
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-04-04 15:37:18 +0200

    creation of custom exception

---
+++ src/klodnicki/robotv2/exception/RobotNotTurnedOnException.java
@@ -0,0 +1,11 @@
+package klodnicki.robotv2.exception;
+
+public class RobotNotTurnedOnException extends Exception{
+
+    private static final String MESSAGE = "Robot is not turned on.";
+
+    public RobotNotTurnedOnException() {
+        super(MESSAGE);
+
+    }
+}
