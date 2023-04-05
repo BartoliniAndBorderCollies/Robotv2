c34976f477383795fe838e1855f6c4bbcf70f420
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-04-05 08:52:09 +0200

    correction of numeration

--- src/klodnicki/robotv2/ScreenPrinter.java
+++ src/klodnicki/robotv2/ScreenPrinter.java
@@ -12,15 +12,15 @@ public class ScreenPrinter {
                 Hello, welcome to Robot command center.
                                 
                 What do you want to do? Type the answer:
-                0. Create the robot.
-                1. Create a charger to be able to recharge battery.
-                2. Turn on the robot.
-                3. Turn off the robot.
-                4. Plug in the robot and start charging.
-                5. Unplug the robot from the charger.
-                6. Move the robot.
-                7. Skip the turn.
-                8. Close the application.
+                1. Create the robot.
+                2. Create a charger to be able to recharge battery.
+                3. Turn on the robot.
+                4. Turn off the robot.
+                5. Plug in the robot and start charging.
+                6. Unplug the robot from the charger.
+                7. Move the robot.
+                8. Skip the turn.
+                9. Close the application.
                                 
                 """);
     }
