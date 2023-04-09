f9e13da47bdaab592811c65890db9ee8acba1034
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-04-09 08:22:25 +0200

    correction of malformed

--- src/klodnicki/robotv2/exception/MaximumEnergyLevelException.java
+++ src/klodnicki/robotv2/exception/MaximumEnergyLevelException.java
@@ -4,11 +4,11 @@ import java.security.MessageDigest;
 
 public class MaximumEnergyLevelException extends Exception{
 
-    private static final String MESSAGE = "Maximum energy level of robot %s is 100%.";
+    private static final String MESSAGE = "Maximum energy level of robot %s is 100 percentages.";
 
 
     public MaximumEnergyLevelException(String name) {
-        super(String.format(MESSAGE, name)); //TODO: czemu malformed?
+        super(String.format(MESSAGE, name));
 
     }
 }
