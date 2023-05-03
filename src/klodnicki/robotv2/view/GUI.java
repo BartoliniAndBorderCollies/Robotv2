d0b8c73c72c0dedb0c7dcdf1988573eca3404c54
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-03 19:01:17 +0200

    cleaning up GUI

--- src/klodnicki/robotv2/view/GUI.java
+++ src/klodnicki/robotv2/view/GUI.java
@@ -1,26 +1,22 @@
 package klodnicki.robotv2.view;
 
-import klodnicki.robotv2.Main2;
-
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+public class GUI extends JFrame implements ActionListener {
 
-    private JLabel welcomeLabel = new JLabel("Welcome to robot command center. You must login to continue.");
-    private JFrame frame = new JFrame();
-    private JPanel panel = new JPanel();
-    private JLabel userLabel = new JLabel("User name:");
-    private JLabel passwordLabel = new JLabel("Password:");
-    private JTextField textField = new JTextField();
-    private JPasswordField passwordField = new JPasswordField();
-    private JButton buttonLogin = new JButton("Login");
-    private JButton buttonCreateAccount = new JButton("Create account");
-    private JCheckBox showPassword = new JCheckBox("Show Password");
-    private JLabel successLabel = new JLabel("");
-
+    private final JLabel welcomeLabel = new JLabel("Welcome to Robot App. You must login to continue.");
+    private final JFrame frame = new JFrame();
+    private final JPanel panel = new JPanel();
+    private final JLabel userLabel = new JLabel("User name:");
+    private final JLabel passwordLabel = new JLabel("Password:");
+    private final JTextField textField = new JTextField();
+    private final JPasswordField passwordField = new JPasswordField();
+    private final JButton buttonLogin = new JButton("Login");
+    private final JButton buttonReset = new JButton("Reset");
+    private final JCheckBox showPassword = new JCheckBox("Show Password");
+    private final JLabel successLabel = new JLabel("");
 
     public GUI(){
         setupFrame();
@@ -35,22 +31,22 @@ public class GUI implements ActionListener {
         userLabel.setBounds(100, 50, 80, 25);
         passwordLabel.setBounds(100, 80, 80, 25);
         successLabel.setBounds(100, 200, 80, 25);
-        buttonLogin.setBounds(190, 130, 80, 25);
-        buttonCreateAccount.setBounds(300, 130, 150, 25);
+        buttonLogin.setBounds(190, 160, 80, 25);
+        buttonReset.setBounds(300, 160, 150, 25);
         textField.setBounds(200, 50, 165, 25);
         passwordField.setBounds(200, 80, 165, 25);
-        showPassword.setBounds(225, 100, 150, 30);
+        showPassword.setBounds(225, 120, 150, 30);
     }
 
     public void addComponents(){
         frame.add(panel);
         panel.add(userLabel);
         panel.add(textField);
+        panel.add(passwordField);
         panel.add(welcomeLabel);
         panel.add(passwordLabel);
-        panel.add(passwordField);
         panel.add(buttonLogin);
-        panel.add(buttonCreateAccount);
+        panel.add(buttonReset);
         panel.add(successLabel);
         panel.add(showPassword);
     }
@@ -61,24 +57,36 @@ public class GUI implements ActionListener {
         panel.setLayout(null);
     }
 
-    public void showLogin() {
-
-
-    }
-
     public void addActionEvent() {
         buttonLogin.addActionListener(this);
-        buttonCreateAccount.addActionListener(this);
+        buttonReset.addActionListener(this);
         showPassword.addActionListener(this);
     }
 
     @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == buttonCreateAccount) {
-            System.out.println("test");
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == buttonLogin) {
+            String userText = textField.getText();
+            String pwdText = passwordField.getText();
+            if(userText.equals("lala") && pwdText.equals("12345")) {
+                JOptionPane.showMessageDialog(this, "Login successful.");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid user name or password.");
+            }
         }
-        if(actionEvent.getSource() == buttonLogin) {
-            System.out.println("kapusta");
+
+        if (e.getSource() == buttonReset) {
+            textField.setText("");
+            passwordField.setText("");
+        }
+
+        if(e.getSource() == showPassword) {
+            if(showPassword.isSelected()) {
+                passwordField.setEchoChar((char)0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
         }
     }
 }
